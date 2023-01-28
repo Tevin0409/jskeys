84cadf5e1591df4c4f036f9a13c1be5f8effc0b2
unknown <tevinobai0409@gmail.com>
2023-01-29 00:17:55 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx}",
+    "./pages/**/*.{js,ts,jsx}",
+    "./components/**/*.{js,ts,jsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx}",
+  ],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
